c568e3a167203592f05a9986c6798a13ab13da3e
Carlos DIaz <c.diazadriann@gmail.com>
2024-05-20 21:03:47 -0400

    Coker

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,17 +3,19 @@ import * as joi from 'joi';
 
 interface EnvVariables {
   PORT: number;
-  PRODUCTS_MS_HOST: string;
-  PRODUCTS_MS_PORT: number;
+  //PRODUCTS_MS_HOST: string;
+  //PRODUCTS_MS_PORT: number;
+  DATABASE_URL: string;
   NATS_SERVERS: string[];
 }
 
 const envsSchema = joi
   .object({
     PORT: joi.number().required(),
-    PRODUCTS_MS_HOST: joi.string().required(),
-    PRODUCTS_MS_PORT: joi.number().required(),
+    //PRODUCTS_MS_HOST: joi.string().required(),
+    //PRODUCTS_MS_PORT: joi.number().required(),
     NATS_SERVERS: joi.array().items(joi.string()).required(),
+    DATABASE_URL: joi.string().required(),
   })
   .unknown(true);
 
@@ -30,7 +32,8 @@ const enVars: EnvVariables = value;
 
 export const envs = {
   PORT: enVars.PORT,
-  PRODUCTS_MS_HOST: enVars.PRODUCTS_MS_HOST,
-  PRODUCTS_MS_PORT: enVars.PRODUCTS_MS_PORT,
+  DATABASE_URL: enVars.DATABASE_URL,
+  //PRODUCTS_MS_HOST: enVars.PRODUCTS_MS_HOST,
+  //PRODUCTS_MS_PORT: enVars.PRODUCTS_MS_PORT,
   NATS_SERVERS: enVars.NATS_SERVERS,
 };
